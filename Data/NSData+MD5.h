aaf0db49b49082f62eeef5566c37ed80538c31a0
shuoyihu <shuoyihu@tencent.com>
2017-10-18 11:19:35 +0800

    new
    new

---
+++ Data/NSData+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSData+MD5.h
+//  Panda
+//
+//  Created by Panda on 14-11-18.
+//  Copyright (c) 2014年 Panda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData(MD5)
+
+- (NSString*)md5;
+
+@end
---
+++ Data/NSData+MD5.m
@@ -0,0 +1,43 @@
+//
+//  NSData+MD5.m
+//  Panda
+//
+//  Created by Panda on 14-11-18.
+//  Copyright (c) 2014年 Panda. All rights reserved.
+//
+
+#import "NSData+MD5.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSData(MD5)
+
+//- (NSString*)md5
+//{
+//    unsigned char result[16];
+//    CC_MD5( self.bytes, self.length, result ); // This is the md5 call
+//    return [NSString stringWithFormat:
+//            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+//            result[0], result[1], result[2], result[3],
+//            result[4], result[5], result[6], result[7],
+//            result[8], result[9], result[10], result[11],
+//            result[12], result[13], result[14], result[15]
+//            ];  
+//}
+
+- (NSString*)md5
+{
+    // Create byte array of unsigned chars
+    unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
+    
+    // Create 16 byte MD5 hash value, store in buffer
+    CC_MD5(self.bytes, (CC_LONG)self.length, md5Buffer);
+    
+    // Convert unsigned char buffer to NSString of hex values
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x",md5Buffer[i]];
+    
+    return output;
+}
+
+@end
